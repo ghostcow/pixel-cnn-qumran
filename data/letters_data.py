0d4a250afad8083a2f744824fa4cfe99a6f0c207
Lior Uzan <sintzu@gmail.com>
2017-02-15 21:54:39 +0200

    added labels

--- data/letters_data.py
+++ data/letters_data.py
@@ -83,7 +83,7 @@ class DataLoader(object):
 
         # on intermediate iterations fetch the next batch
         x = self.data[self.p : self.p + n]
-        # y = self.labels[self.p : self.p + n]
+        y = self.labels[self.p : self.p + n]
         m = self.masks[self.p : self.p + n]
         self.p += self.batch_size
 
--- train.py
+++ train.py
@@ -133,20 +133,28 @@ for i in range(args.nr_gpu):
         gen_par = model(xs[i], h_sample[i], ema=ema, dropout_p=0, **model_opt)
         new_x_gen.append(nn.sample_from_discretized_mix_logistic(gen_par, args.nr_logistic_mix))
 def sample_from_model(sess, data):
+#    x_gen = [np.zeros((args.batch_size,) + obs_shape, dtype=np.float32) for i in range(args.nr_gpu)]
     x_gen, masks = data
-    # x_gen = np.zeros_like(x_gen)
-    # masks = np.zeros_like(masks)
-    masks = np.round(masks / 255) # temp fix, this needs to be done at data creation.
+    x_gen = np.cast[np.float32]((x_gen - 127.5) / 127.5) # input to pixelCNN is scaled from uint8 [0,255] to float in range [-1,1]
+    masks = np.cast[np.float32](np.round(masks / 255)) # temp fix, this needs to be done at data creation.
     x_gen = np.split(x_gen, args.nr_gpu)
     masks = np.split(masks, args.nr_gpu)
     for yi in range(obs_shape[0]):
         for xi in range(obs_shape[1]):
             new_x_gen_np = sess.run(new_x_gen, {xs[i]: x_gen[i] for i in range(args.nr_gpu)})
             for i in range(args.nr_gpu):
-                # pdb.set_trace()
-               # x_gen[i][:,yi,xi,:] = new_x_gen_np[i][:,yi,xi,:]
+#                x_gen[i][:,yi,xi,:] = new_x_gen_np[i][:,yi,xi,:]
                 x_gen[i][:,yi,xi,:] = new_x_gen_np[i][:,yi,xi,:]*(1-masks[i][:,yi,xi,:])+x_gen[i][:,yi,xi,:]*masks[i][:,yi,xi,:]
-    return np.concatenate(x_gen, axis=0)
+
+    x_gen = np.concatenate(x_gen, axis=0)
+    masks = np.concatenate(masks, axis=0)
+    # color black fill-in as red, white fill-in as green
+    black_inds = ( (x_gen)*(1-masks) == -1 )
+    white_inds = np.cast[np.bool]( (~black_inds)*(1-masks) )
+    x_gen[black_inds[...,0],0] = 1
+    x_gen[white_inds[...,0],0] = -1
+    x_gen[white_inds[...,2],2] = -1
+    return x_gen
 
 # init & save
 initializer = tf.initialize_all_variables()
@@ -197,37 +205,39 @@ with tf.Session() as sess:
                 saver.restore(sess, ckpt_file)
 
         # train for one epoch
-        train_losses = []
-        for d in tqdm(train_data):
-            feed_dict = make_feed_dict(d)
-            # forward/backward/update model on each gpu
-            lr *= args.lr_decay
-            feed_dict.update({ tf_lr: lr })
-            l,_ = sess.run([bits_per_dim, optimizer], feed_dict)
-            train_losses.append(l)
-        train_loss_gen = np.mean(train_losses)
-
-        # compute likelihood over test data
-        test_losses = []
-        for d in test_data:
-            feed_dict = make_feed_dict(d)
-            l = sess.run(bits_per_dim_test, feed_dict)
-            test_losses.append(l)
-        test_loss_gen = np.mean(test_losses)
-        test_bpd.append(test_loss_gen)
+#        train_losses = []
+#        for d in tqdm(train_data):
+#            feed_dict = make_feed_dict(d)
+#            # forward/backward/update model on each gpu
+#            lr *= args.lr_decay
+#            feed_dict.update({ tf_lr: lr })
+#            l,_ = sess.run([bits_per_dim, optimizer], feed_dict)
+#            train_losses.append(l)
+#        train_loss_gen = np.mean(train_losses)
+#
+#        # compute likelihood over test data
+#        test_losses = []
+#        for d in test_data:
+#            feed_dict = make_feed_dict(d)
+#            l = sess.run(bits_per_dim_test, feed_dict)
+#            test_losses.append(l)
+#        test_loss_gen = np.mean(test_losses)
+#        test_bpd.append(test_loss_gen)
 
         # log progress to console
-        print("Iteration %d, time = %ds, train bits_per_dim = %.4f, test bits_per_dim = %.4f" % (epoch, time.time()-begin, train_loss_gen, test_loss_gen))
-        sys.stdout.flush()
+#        print("Iteration %d, time = %ds, train bits_per_dim = %.4f, test bits_per_dim = %.4f" % (epoch, time.time()-begin, train_loss_gen, test_loss_gen))
+#        sys.stdout.flush()
 
         if epoch % args.save_interval == 0:
 
             # generate samples from the model
+            print('generating samples from model...', end='', flush=True)
             sample_x = sample_from_model(sess, test_data.next())
             img_tile = plotting.img_tile(sample_x[:int(np.floor(np.sqrt(args.batch_size*args.nr_gpu))**2)], aspect_ratio=1.0, border_color=1.0, stretch=True)
             img = plotting.plot_img(img_tile, title=args.data_set + ' samples')
             plotting.plt.savefig(os.path.join(args.save_dir,'%s_sample%d.png' % (args.data_set, epoch)))
             plotting.plt.close('all')
+            print('done')
 
             # save params
             saver.save(sess, args.save_dir + '/params_' + args.data_set + '.ckpt')
