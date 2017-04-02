fixed up rotation biz a notch

data/letters_data.py
@@ -57,6 +57,9 @@ class DataLoader(object):
             self.data = self.data[inds]
             self.labels = self.labels[inds]
             self.masks = self.masks[inds]
+        elif self.test and self.rotation is None:
+            print('Error!!! Cannot test without specifying rotation!!')
+            sys.exit(-1)
         
         self.p = 0 # pointer to where we are in iteration
         self.rng = np.random.RandomState(1) if rng is None else rng
@@ -65,7 +68,7 @@ class DataLoader(object):
         return self.data.shape[1:]
 
     def get_num_labels(self):
-        return 4
+        return 8
 #        return np.amax(self.labels) + 1
 
     def set_batch_size(self, n):

train.py
@@ -137,76 +137,56 @@ for i in range(args.nr_gpu):
     with tf.device('/gpu:%d' % i):
         gen_par = model(xs[i], h_sample[i], ema=ema, dropout_p=0, **model_opt)
         new_x_gen.append(nn.sample_from_discretized_mix_logistic(gen_par, args.nr_logistic_mix))
-def sample_from_model(sess, data, s=0):
+def sample_from_model(sess):
     x_gen = [np.zeros((args.batch_size,) + obs_shape, dtype=np.float32) for i in range(args.nr_gpu)]
-#    x_gen, labels, masks, k = data
-#    x_gen = np.cast[np.float32]((x_gen - 127.5) / 127.5) # input to pixelCNN is scaled from uint8 [0,255] to float in range [-1,1]
-#    x_sample = np.rot90(x_gen[s], k=-k, axes=(0,1)).copy()
-#    m_sample = np.rot90(masks[s], k=-k, axes=(0,1)).copy()
-#    
-#    label = labels[s]
-
-
-#    x_gen[:,...] = x_sample
-#    masks[:,...] = m_sample
-#    for i,k in enumerate(np.concatenate(y_sample, axis=0)):
-#        if i==0:
-#            continue
-#        x_gen[i] = np.rot90(x_gen[i], k=k, axes=(0,1))
-#        masks[i] = np.rot90(masks[i], k=k, axes=(0,1))
-#    x_gen = np.split(x_gen, args.nr_gpu)
-#    masks = np.split(masks, args.nr_gpu)
     for yi in range(obs_shape[0]):
         for xi in range(obs_shape[1]):
             new_x_gen_np = sess.run(new_x_gen, {xs[i]: x_gen[i] for i in range(args.nr_gpu)})
             for i in range(args.nr_gpu):
                 x_gen[i][:,yi,xi,:] = new_x_gen_np[i][:,yi,xi,:]
-#                x_gen[i][:,yi,xi,:] = new_x_gen_np[i][:,yi,xi,:]*(1-masks[i][:,yi,xi,:])+x_gen[i][:,yi,xi,:]*masks[i][:,yi,xi,:]
     return np.concatenate(x_gen, axis=0)
-#    x_gen = np.concatenate(x_gen, axis=0)
-#    masks = np.concatenate(masks, axis=0)
-    # color black fill-in as red, white fill-in as green
-#    black_inds = ( (x_gen)*(1-masks) == -1 )
-#    white_inds = np.cast[np.bool]( (~black_inds)*(1-masks) )
-#    x_gen[black_inds[...,0],0] = 1
-#    x_gen[white_inds[...,0],0] = -1
-#    x_gen[white_inds[...,2],2] = -1
-    # first sample is the real sample
-#    x_gen[0] = x_sample
-    # rotate back
-#    x_gen = np.concatenate(x_gen, axis=0)
-#    x_gen_unrot = x_gen.copy()
-#    for i,k in enumerate(np.concatenate(y_sample, axis=0)):
-#        if i==0:
-#            continue
-#        x_gen_unrot[i] = np.rot90(x_gen_unrot[i], k=-k, axes=(0,1))
-#    return x_gen, x_gen_unrot, label
 
 
 # init & save
 initializer = tf.global_variables_initializer()
 saver = tf.train.Saver()
 
+def flip_rotate(x, y):
+    '''
+    flips and/or rotates a single image according to label y
+    y is encoded to represent flips and rotations
+    y \in {0..7}
+    flip indicator = y // 4
+    rotation angle = (y % 4) * 90 degrees
+    '''
+    flip = y // 4
+    if flip == 1:
+        x = np.flip(x, len(x.shape)-2)
+    x = np.rot90(x, k= y % 4)
+    return x
+
+def adaptive_rotation(x, y):
+    if type(y)==int:
+        x = flip_rotate(x, y)
+    else:
+        for j in range(len(y)):
+            x[j] = flip_rotate(x[j], y[j])
+    return x
+
 # turn numpy inputs into feed_dict for use with tensorflow
 def make_feed_dict(data, init=False):
+    '''
+    contract: if no labels then rotation must be specified
+    '''
     if type(data) is tuple and len(data)==2:
         x,m = data
         y = None
+        x = adaptive_rotation(x, args.rotation)
     elif type(data) is tuple and len(data)==3:
         x,y,m = data
-    else:
-        x = data
-        y = None
+        x = adaptive_rotation(x, y)
+
     x = np.cast[np.float32]((x - 127.5) / 127.5) # input to pixelCNN is scaled from uint8 [0,255] to float in range [-1,1]
-    
-    # TODO: FIX THIS!!!! (randomly) rotate batch 
-    if args.rotation is None:
-        k = np.random.randint(4)
-        y.fill(k)
-    else:
-        k = args.rotation
-    x = np.rot90(x, k=k, axes=(1,2))
-    m = np.rot90(m, k=k, axes=(1,2))
     
     if init:
         feed_dict = {x_init: x}
@@ -223,6 +203,7 @@ def make_feed_dict(data, init=False):
 # //////////// perform training //////////////
 if not os.path.exists(args.save_dir):
     os.makedirs(args.save_dir)
+    
 # save hyperparms to file
 with open(os.path.join(args.save_dir,'hyperparams.txt'),'w') as f:
     f.write(json.dumps(vars(args), indent=4, separators=(',',':')))
@@ -233,7 +214,6 @@ test_loss_gen = np.inf
 lr = args.learning_rate
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
 
 # early stopping params
 patience = 150
@@ -247,60 +227,53 @@ stop_training = False
 with tf.Session(config=config) as sess:
     for epoch in range(args.max_epochs):
         begin = time.time()
-
+        
         # init
         if epoch == 0:
+            # manually retrieve exactly init_batch_size examples
+            feed_dict = make_feed_dict(
+                train_data.next(args.init_batch_size), init=True)
+            train_data.reset()  # rewind the iterator back to 0 to do one full epoch
+            sess.run(initializer, feed_dict)
             print('initializing the model...')
-            if not args.just_gen:
-                feed_dict = make_feed_dict(train_data.next(args.init_batch_size), init=True) # manually retrieve exactly init_batch_size examples
-                train_data.reset()  # rewind the iterator back to 0 to do one full epoch
-                sess.run(initializer, feed_dict)
-            elif args.load_params:
+            if args.load_params:
                 ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
                 print('restoring parameters from', ckpt_file)
                 saver.restore(sess, ckpt_file)
-            else:
-                print("error, can't generate samples without loading params.")
-                sys.exit(-1)
-        
-        if not args.just_gen:
-            # train for one epoch
-            train_losses = []
-            for d in tqdm(train_data):
-                feed_dict = make_feed_dict(d)
-                # forward/backward/update model on each gpu
-                lr *= args.lr_decay
-                feed_dict.update({ tf_lr: lr })
-                l,_ = sess.run([bits_per_dim, optimizer], feed_dict)
-                train_losses.append(l)
-            train_loss_gen = np.mean(train_losses)
-    
-            # compute likelihood over test data
-            test_losses = []
-            for d in test_data:
-                feed_dict = make_feed_dict(d)
-                l = sess.run(bits_per_dim_test, feed_dict)
-                test_losses.append(l)
-            test_loss_gen = np.mean(test_losses)
-            test_bpd.append(test_loss_gen)
-    
-            # log progress to console
-            print("Iteration %d, time = %ds, train bits_per_dim = %.4f, test bits_per_dim = %.4f" % (epoch, time.time()-begin, train_loss_gen, test_loss_gen))
-            sys.stdout.flush()
+
+        # train for one epoch
+        train_losses = []
+        for d in tqdm(train_data):
+            feed_dict = make_feed_dict(d)
+            # forward/backward/update model on each gpu
+            lr *= args.lr_decay
+            feed_dict.update({ tf_lr: lr })
+            l,_ = sess.run([bits_per_dim, optimizer], feed_dict)
+            train_losses.append(l)
+        train_loss_gen = np.mean(train_losses)
+
+        # compute likelihood over test data
+        test_losses = []
+        for d in test_data:
+            feed_dict = make_feed_dict(d)
+            l = sess.run(bits_per_dim_test, feed_dict)
+            test_losses.append(l)
+        test_loss_gen = np.mean(test_losses)
+        test_bpd.append(test_loss_gen)
+
+        # log progress to console
+        print("Iteration %d, time = %ds, train bits_per_dim = %.4f, test bits_per_dim = %.4f" % (epoch, time.time()-begin, train_loss_gen, test_loss_gen))
+        sys.stdout.flush()
                    
         if epoch % args.gen_interval == 0 and epoch > 0:
             # generate samples from the model
             print('generating samples from model...')
-            data = test_data.next()
-            test_data.reset()
-
             def print_samples(sample_x, suffix=''):
                 img_tile = plotting.img_tile(sample_x[:int(np.floor(np.sqrt(args.batch_size*args.nr_gpu))**2)], aspect_ratio=1.0, border_color=1.0, stretch=True).squeeze()
-                img = plotting.plot_img(img_tile, title=args.data_set + ' samples')
+                plotting.plot_img(img_tile, title=args.data_set + ' samples')
                 plotting.plt.savefig(os.path.join(args.save_dir,'%s_sample%d%s.png' % (args.data_set, epoch, suffix)))
                 plotting.plt.close('all')
-
-            sample_x = sample_from_model(sess, data)
+            sample_x = sample_from_model(sess)
             print_samples(sample_x)
             print('done.')
             

train_par.py
@@ -138,12 +138,15 @@ bits_per_dim_test = [loss_gen_test[i]/(np.log(2.)*np.prod(obs_shape)*args.batch_
 
 # sample from the model
 new_x_gen = []
-for i in range(args.nr_gpu):
+for i in range(1):
     with tf.device('/gpu:%d' % i):
-        gen_par = model(xs[i], h_sample[i], ema=ema, dropout_p=0, **model_opt)
-        new_x_gen.append(nn.sample_from_discretized_mix_logistic(gen_par, args.nr_logistic_mix))
+        for j in range(2):
+            k = 2*i+j
+            with tf.variable_scope('model%d' % k, reuse=True):  
+                gen_par = model(xs[i], h_sample[i], ema=ema, dropout_p=0, **model_opt)
+                new_x_gen.append(nn.sample_from_discretized_mix_logistic(gen_par, args.nr_logistic_mix))
 def sample_from_model(sess, data, s=0):
-    x_gen = [np.zeros((args.batch_size,) + obs_shape, dtype=np.float32) for i in range(args.nr_gpu)]
+    x_gen = [np.zeros((args.batch_size,) + obs_shape, dtype=np.float32) for i in range(args.nr_gpu*2)]
 #    x_gen, labels, masks, k = data
 #    x_gen = np.cast[np.float32]((x_gen - 127.5) / 127.5) # input to pixelCNN is scaled from uint8 [0,255] to float in range [-1,1]
 #    x_sample = np.rot90(x_gen[s], k=-k, axes=(0,1)).copy()
@@ -163,7 +166,7 @@ def sample_from_model(sess, data, s=0):
 #    masks = np.split(masks, args.nr_gpu)
     for yi in range(obs_shape[0]):
         for xi in range(obs_shape[1]):
-            new_x_gen_np = sess.run(new_x_gen, {xs[i]: x_gen[i] for i in range(args.nr_gpu)})
+            new_x_gen_np = sess.run(new_x_gen, {xs[i]: x_gen[i] for i in range(args.nr_gpu*2)})
             for i in range(args.nr_gpu):
                 x_gen[i][:,yi,xi,:] = new_x_gen_np[i][:,yi,xi,:]
 #                x_gen[i][:,yi,xi,:] = new_x_gen_np[i][:,yi,xi,:]*(1-masks[i][:,yi,xi,:])+x_gen[i][:,yi,xi,:]*masks[i][:,yi,xi,:]
@@ -218,11 +221,11 @@ def make_feed_dict(data, init=False):
         if y is not None and args.class_conditional:
             feed_dict.update({y_init: y})
     else:
-        x = np.split(x, args.nr_gpu)
-        feed_dict = {xs[i]: x[i] for i in range(args.nr_gpu)}
+        x = np.split(x, args.nr_gpu*2)
+        feed_dict = {xs[i]: x[i] for i in range(args.nr_gpu*2)}
         if y is not None and args.class_conditional:
             y = np.split(y, args.nr_gpu)
-            feed_dict.update({ys[i]: y[i] for i in range(args.nr_gpu)})
+            feed_dict.update({ys[i]: y[i] for i in range(args.nr_gpu*2)})
     return feed_dict
 
 # //////////// perform training //////////////