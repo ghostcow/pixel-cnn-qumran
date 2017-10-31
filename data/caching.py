2219427192876df037b4fe32f2e5b8f648e2a84b
Lior Uzan <sintzu@gmail.com>
2017-10-31 22:28:21 +0200

    finished first version of caching.py

--- data/caching.py
+++ data/caching.py
@@ -15,17 +15,17 @@ def parse_resize(imgs, s=32):
             h, w = im.shape[0], im.shape[1]
         if w>s:
             ar = s/w
-            im = sm.imresize(im,ar,interp='bicubic')
+            im = sm.imresize(im, ar, interp='bicubic')
             h, w = im.shape[0], im.shape[1]
         img_rsz[j,(c-h//2):(c+h//2+h%2),(c-w//2):(c+w//2+w%2),:]=im
     return img_rsz
 
-def get_image_tensor(file_list):
-    imgs = [sm.imread(file, mode='RGB') for file in file_list]
-    return parse_resize(imgs)
+def get_image_list(path):
+    return sorted(glob(os.path.join(path, '*.jpg')))
 
-def build_cache(path):
-    return sorted(glob(os.path.join(path, 'images', '*.jpg')))
+def get_image_tensor(image_list):
+    imgs = [sm.imread(file, mode='RGB') for file in image_list]
+    return parse_resize(imgs)
 
 def load_cache(path):
     cache_file = os.path.join(path, 'cache.pkl')
@@ -35,21 +35,33 @@ def load_cache(path):
     else:
         return None
 
-def save_cache(path, file_list, data):
+def save_cache(path, letter_list, letter_tensor, mask_list, mask_tensor):
     cache_file = os.path.join(path, 'cache.pkl')
     with open(cache_file, 'wb') as f:
         pkl.dump({
-            'file_list': file_list,
-            'images': data,
-            'masks': None
+            'letter_list': letter_list,
+            'letter_tensor': letter_tensor,
+            'mask_list': mask_list,
+            'mask_tensor': mask_tensor
         }, f)
 
+'''
+The data is structured as follows:
+data/letters - place the output of the letter spotting algorithm here
+data/masks - (optional) place the masks you created for the data here
+'''
 def load_data(path):
-    file_list = build_cache(path)
+    letter_list = get_image_list(os.path.join(path, 'images'))
+    mask_list = get_image_list(os.path.join(path, 'masks'))
     cache = load_cache(path)
-    if cache is None or set(cache['file_list']) != set(file_list):
-        images = get_image_tensor(file_list)
-        save_cache(path, file_list, images)
-        return data
+    if cache is None \
+            or set(cache['letter_list']) != set(letter_list) \
+            or set(cache['mask_list']) != set(mask_list):
+        letter_tensor = get_image_tensor(letter_list)
+        mask_tensor = get_image_tensor(mask_list)
+        save_cache(path, letter_list, letter_tensor, mask_list, mask_tensor)
     else:
-        return cache['images']
+        letter_tensor = cache['letter_tensor']
+        mask_tensor = cache['mask_tensor']
+
+    return letter_tensor, mask_tensor
